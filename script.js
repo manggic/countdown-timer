924555863811a824f7efb250fd4e966204182973
manggic <manishmahto198@gmail.com>
2020-12-28 23:43:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log('in JS ');
+
+
+
+setInterval( () => {
+    let d1 = new Date()  // month/day/year
+    let d2 = new Date('1/1/2021')
+    
+    console.log(d2.toLocaleTimeString());
+    let diff = (d2 - d1)  //miliseconds
+    
+    console.log('miliseconds', diff);
+    let timeInSecond  = Math.floor(diff / 1000)     // milisec to second
+    
+    //step1 
+       
+       let someHours = Math.floor(timeInSecond / 3600)
+       let someSeconds =   timeInSecond % 3600
+    
+    // step2
+    
+       let finalMinutes =  Math.floor(someSeconds / 60)
+       let finalSecond = someSeconds % 60
+    //step3
+       let finalDays = Math.floor( someHours / 24 )
+       let finalHours = someHours % 24
+       
+      console.log(finalDays, finalHours,  finalMinutes, finalSecond); 
+                    
+ document.getElementById('days').innerHTML = finalDays
+document.getElementById('hours').innerHTML = finalHours
+document.getElementById('mins').innerHTML = finalMinutes
+document.getElementById('secs').innerHTML = finalSecond
+
+//console.log(finalDay, hour , minute, second);
+
+} , 500 )
+
+
